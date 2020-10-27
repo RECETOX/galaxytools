c83009184c64c4178e132f9e12170818c52f7db7
Martin Cech <marten@bx.psu.edu>
2020-10-27 10:25:42 +0100

    add various fixes and improvements
    namely:
    * construct full export path correctly by joining path components
    * create the directory in case it does not exist (which it usually doesn't)
    * drop unncessary changing of dirs
    * refactor static option
    * specify dependency on python
    * correct path to user env for bash

--- tools/export_to_path/export_to_path.py
+++ tools/export_to_path/export_to_path.py
@@ -1,4 +1,4 @@
-#!/usrexpo/bin/env python
+#!/usr/bin/env python
 
 from __future__ import print_function
 import optparse
@@ -9,32 +9,23 @@ import shutil
 import sys
 
 '''
-Inspired by
-https://github.com/TGAC/earlham-galaxytools/tree/master/tools/export_to_cluster/
+Inspired by @nsoranzo's https://github.com/TGAC/earlham-galaxytools/tree/master/tools/export_to_cluster/
 '''
 
 parser = optparse.OptionParser()
 parser.add_option('-d', '--export_dir', help='Directory where to export the datasets')
-parser.add_option('-p', '--dir_prefix', help='How the export dir should start')
 (options, args) = parser.parse_args()
 if not options.export_dir:
     parser.error('Export directory cannot be empty')
-if len(args) < 3:
-    parser.error('Require at least two arguments')
-if len(args) % 3 != 0:
-    parser.error('Require an even number of arguments')
 
-source_directory = os.getcwd()
-dir_prefix = os.chdir(options.dir_prefix)
-options.export_dir += '/mzML_profile'
-real_export_dir = os.path.realpath(options.export_dir)
-dir_prefix = options.dir_prefix.rstrip(os.sep)
+dir_prefix = "/mnt/sally"
+dir_suffix = 'mzML_profile'
+full_export_dir = os.path.join(dir_prefix, options.export_dir.lstrip(os.sep), dir_suffix)
+real_export_dir = os.path.realpath(full_export_dir)
 if not real_export_dir.startswith(dir_prefix):
-    raise Exception("'%s' must be a subdirectory of '%s'" % (options.export_dir, dir_prefix))
+    raise Exception("'%s' must be a subdirectory of '%s'" % (real_export_dir, dir_prefix))
 if not os.path.exists(real_export_dir):
-    raise Exception("'%s' directory does not exist or it is not accessible by the Galaxy user" % options.export_dir)
-if not os.path.isdir(real_export_dir):
-    raise Exception("'%s' is not a directory" % options.export_dir)
+    os.makedirs(real_export_dir)
 
 dataset_paths = args[::3]
 dataset_names = args[1::3]
@@ -52,7 +43,6 @@ for dp, dn, de in zip(dataset_paths, dataset_names, dataset_exts):
         raise Exception("Error copying dataset '%s' to '%s'. Cannot overwrite existing dataset" % (dn, dest))
     else:
         try:
-            os.chdir(source_directory)
             shutil.copy2(dp, dest)
             print("Dataset '%s' copied to '%s'" % (dn, dest))
         except Exception as e:
