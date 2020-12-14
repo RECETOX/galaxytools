7e3400296dcfecdcb7142532609b435c9b217b2d
maximskorik <maximskoryk@gmail.com>
2020-12-14 11:37:46 +0100

    refactor code to pass flake8 style requirements

--- tools/hdf_converter/hdf_converter.py
+++ tools/hdf_converter/hdf_converter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import pandas as pd
 import optparse
+import pandas as pd
 import sys
 import warnings
 
@@ -20,16 +20,18 @@ def extract_samples(table, num_samples, idx):
     rt_idx_name = table.columns.tolist()[rt_idx]
     table.dropna(subset=[rt_idx_name], inplace=True)
     sample_name = table.columns.tolist()[intensity_idx].split('.')[1]
-    mzrt = table['mz'].map(str) + '_' + table.iloc[:,rt_idx].map(str)
-    intensity = table.iloc[:,intensity_idx]
+    mzrt = table['mz'].map(str) + '_' + table.iloc[:, rt_idx].map(str)
+    intensity = table.iloc[:, intensity_idx]
     mzrt_intensity = {'mz_rt': mzrt, sample_name: intensity}
-    mzrt_intensity = pd.DataFrame(mzrt_intensity, columns=['mz_rt', sample_name])
+    mzrt_intensity = pd.DataFrame(
+        mzrt_intensity, columns=['mz_rt', sample_name]
+        )
     mzrt_intensity.set_index('mz_rt', inplace=True)
     return mzrt_intensity
 
 
 def join_samples(table):
-    num_samples = int((len(table.columns.tolist()) - 4) / 2) #4 default columns: mz,rt,mz_min,mz_max. The rest is intensity and rt columns for each sample
+    num_samples = int((len(table.columns.tolist()) - 4) / 2)  # 4 default columns: mz,rt,mz_min,mz_max. The rest is intensity and rt columns for each sample
     RamClustr_data = pd.DataFrame(columns=['mz_rt'])
     for sample in range(num_samples):
         sample_data = extract_samples(table, num_samples, sample)
@@ -38,11 +40,11 @@ def join_samples(table):
 
 
 def convert_to_RamClustR(RamClustr_data):
-    RamClustr_data.fillna(0,inplace=True)
-    RamClustr_data.rename(columns={'mz_rt':'sample'},inplace=True)
-    RamClustr_data.set_index('sample',inplace=True)
+    RamClustr_data.fillna(0, inplace=True)
+    RamClustr_data.rename(columns={'mz_rt': 'sample'}, inplace=True)
+    RamClustr_data.set_index('sample', inplace=True)
     RamClustr_data_transposed = RamClustr_data.transpose()
-    RamClustr_data_transposed.index.rename('sample',inplace=True)
+    RamClustr_data_transposed.index.rename('sample', inplace=True)
     return RamClustr_data_transposed
 
 
@@ -51,12 +53,13 @@ def main():
         aplcms_table = pd.read_hdf(options.dataframe, options.table, errors='None')
     except KeyError:
         sys.exit("Selected table does not exist in HDF dataframe")
-    
+
     RamClutsr_data = join_samples(aplcms_table)
     RamClustr_data = convert_to_RamClustR(RamClutsr_data)
     output = args[0]
     RamClustr_data.to_csv(output, sep=';')
-    print("Table '{}' of HDF dataset is converted to csv for RamClutsR".format(options.table, options.dataframe))
+    print("Table '{}' of HDF dataset is converted to csv for RamClutsR".format(options.table))
+
 
 if __name__ == "__main__":
     main()
