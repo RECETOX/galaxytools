a3dd7c03bd1399a743361f8a4134b40bba9e34c0
maximskorik <maximskoryk@gmail.com>
2021-02-15 19:37:22 +0100

    add "exclude group" feature

--- tools/waveica/waveica_wrapper.R
+++ tools/waveica/waveica_wrapper.R
@@ -7,14 +7,17 @@ waveica <- function(
     K,
     t,
     t2,
-    alpha
+    alpha,
+    group_label
 ) {
 
+    # get input from the Galaxy
     input <- assign_data(data, batch, group)
     features_data <- input[[1]]
     batch_data <- input[[2]]
     group_data <- input[[3]]
 
+    # run WaveICA
     normalized_data <- WaveICA::WaveICA(
         data = features_data,
         wf = define_wt_function(wfil, wlen),
@@ -25,19 +28,25 @@ waveica <- function(
         t2 = t2,
         alpha = alpha
         )
-        return(normalized_data)
+    
+    # get spectra from the list of 1 item
+    normalized_data <- normalized_data$data_wave
+
+    # exclude a group if label is provided
+    if (!is.null(group_label)) {
+        normalized_data <- exclude_group(normalized_data, group_data, group_label)
+    }
+
+    return(normalized_data)
 }
 
 
-# This is not ideal since R stores 2 copies of each dataset. Will be able to fix that once we use other input than Rdata
-assign_data <- function(
-    data,
-    batch,
-    group
-) {
+# This is not ideal since R will store 2 copies of each dataset. Will fix that once we use other input than Rdata
+assign_data <- function(data, batch, group) {
     assign('features_data', get(load(data)))
     assign('batch_data', get(load(batch)))
     
+    # handle optional "Group" input
     if (!is.null(group)) {
         assign('group_data', get(load(group)))
     } else {
@@ -49,24 +58,40 @@ assign_data <- function(
 }
 
 
-# Creates appropriate input for R wavelets function
-define_wt_function <- function(
-    wfil,
-    wlen
-) {
+# Create appropriate input for R wavelets function
+define_wt_function <- function(wfil, wlen) {
+    
     wf=paste(wfil,wlen,sep="")
 
+    # exception to the wavelet function
     if (wf == "d2") {
-        wf <- "haar" # exception to wavelet function
+        wf <- "haar"
     }
     
     return(wf)
 }
 
 
-# Stores output of WaveICA 
+# Exclude certain group (e.g. blank) from a dataframe
+exclude_group <- function(normalized_data, group_data, group_label) {
+        
+    if (is.null(group_data)) {
+        cat("Cannot exclude a group as no group data was provided.\n")
+    }
+    else {
+        idx_to_exclude <- which(group_data %in% group_label)
+        data_without_group <- normalized_data[-c(idx_to_exclude),]
+
+        msg <- paste("Group with label", group_label, "has been excluded from the dataframe.\n")
+        cat(msg)
+
+        return(data_without_group)
+    }
+}
+
+
+# Store output of WaveICA 
 store_data <- function(normalized_data, output) {
-    normalized_data <- normalized_data$data_wave
     save(normalized_data,file=output)
-    print("Normalization has been completed.")
+    cat("Normalization has been completed.\n")
 }
