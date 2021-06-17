2d0efd083624ad35f1c01430e136db59c99e8c20
Martin Cech <marten@bx.psu.edu>
2021-06-17 18:10:16 +0200

    lint R

--- tools/recetox_aplcms/main.R
+++ tools/recetox_aplcms/main.R
@@ -1,49 +1,49 @@
 library(recetox.aplcms)
 
-save_extracted_features <- function (df, filename) {
+save_extracted_features <- function(df, filename) {
   df <- as.data.frame(df)
-  columns <- c('mz', 'pos', 'sd1', 'sd2', 'area')
+  columns <- c("mz", "pos", "sd1", "sd2", "area")
   arrow::write_parquet(df[columns], filename)
 }
 
-save_feature_sample_table <- function (df, filename) {
-  columns <- c('feature', 'mz', 'rt', 'sample', 'sample_rt', 'sample_intensity')
+save_feature_sample_table <- function(df, filename) {
+  columns <- c("feature", "mz", "rt", "sample", "sample_rt", "sample_intensity")
   arrow::write_parquet(df[columns], filename)
 }
 
-known_table_columns <- function () {
-  c('chemical_formula', 'HMDB_ID', 'KEGG_compound_ID', 'mass', 'ion.type',
-    'm.z', 'Number_profiles_processed', 'Percent_found', 'mz_min', 'mz_max',
-    'RT_mean', 'RT_sd', 'RT_min', 'RT_max', 'int_mean(log)', 'int_sd(log)',
-    'int_min(log)', 'int_max(log)')
+known_table_columns <- function() {
+  c("chemical_formula", "HMDB_ID", "KEGG_compound_ID", "mass", "ion.type",
+    "m.z", "Number_profiles_processed", "Percent_found", "mz_min", "mz_max",
+    "RT_mean", "RT_sd", "RT_min", "RT_max", "int_mean(log)", "int_sd(log)",
+    "int_min(log)", "int_max(log)")
 }
 
-save_known_table <- function (df, filename) {
+save_known_table <- function(df, filename) {
   columns <- known_table_columns()
   arrow::write_parquet(df[columns], filename)
 }
 
-read_known_table <- function (filename) {
+read_known_table <- function(filename) {
   arrow::read_parquet(filename, col_select = known_table_columns())
 }
 
 save_pairing <- function(df, filename) {
-  write.table(df, filename, row.names = FALSE, col.names = c('new', 'old'))
+  write.table(df, filename, row.names = FALSE, col.names = c("new", "old"))
 }
 
-save_extracted_features_as_collection <- function (dfs, filenames) {
+save_extracted_features_as_collection <- function(dfs, filenames) {
   filenames <- tools::file_path_sans_ext(filenames)
-  filenames <- paste0('extracted-', filenames, '.parquet')
+  filenames <- paste0("extracted-", filenames, ".parquet")
   mapply(save_extracted_features, dfs, filenames)
 }
 
-save_corrected_features_as_collection <- function (dfs, filenames) {
+save_corrected_features_as_collection <- function(dfs, filenames) {
   filenames <- tools::file_path_sans_ext(filenames)
-  filenames <- paste0('corrected-', filenames, '.parquet')
+  filenames <- paste0("corrected-", filenames, ".parquet")
   mapply(save_extracted_features, dfs, filenames)
 }
 
-unsupervised_main <- function (sample_files, aligned_file, recovered_file, ...) {
+unsupervised_main <- function(sample_files, aligned_file, recovered_file, ...) {
   sample_files <- sort_samples_by_acquisition_number(sample_files)
 
   res <- unsupervised(filenames = sample_files, ...)
@@ -55,7 +55,7 @@ unsupervised_main <- function (sample_files, aligned_file, recovered_file, ...) 
   save_feature_sample_table(res$recovered_feature_sample_table, recovered_file)
 }
 
-hybrid_main <- function (sample_files, known_table_file, updated_known_table_file, pairing_file, aligned_file, recovered_file, ...) {
+hybrid_main <- function(sample_files, known_table_file, updated_known_table_file, pairing_file, aligned_file, recovered_file, ...) {
   sample_files <- sort_samples_by_acquisition_number(sample_files)
 
   known <- read_known_table(known_table_file)
