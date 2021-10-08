1bf8254f00fb55b570e8faba20e3573a39ed3940
hechth <helge.hecht@recetox.muni.cz>
2021-10-08 09:38:27 +0200

    Removed old code and reformatted sctipts; implemented cli via click

--- tools/matchms/formatter.py
+++ tools/matchms/formatter.py
@@ -1,38 +1,71 @@
+import click
 from pandas import read_csv, DataFrame
 
 
-matches_filename = "matches.csv"
-scores_filename = "scores.csv"
-
-matches_threshold = 3
-scores_threshold = 0.6
-
-num_top_matches = 3
+def create_long_table(data: DataFrame, value_id: str):
+    return data.transpose().melt(ignore_index=False, var_name='compound', value_name=value_id)
 
 
-def create_long_table(data, value_id):
-    return data.transpose().melt(ignore_index = False, var_name = 'compound', value_name = value_id)
-
-def join_df(x, y, on = [], how = "inner"):    
-    df_x = x.set_index([ x.index ] + on)
-    df_y = y.set_index([ y.index ] + on)
-    combined = df_x.join(df_y, how = how)
+def join_df(x: DataFrame, y: DataFrame, on=[], how="inner") -> DataFrame:
+    df_x = x.set_index([x.index] + on)
+    df_y = y.set_index([y.index] + on)
+    combined = df_x.join(df_y, how=how)
     return combined
 
-def get_top_k_matches(data, k):
-    return data.groupby(level = 0, group_keys = False).apply(DataFrame.nlargest, n = k, columns = ['score'])
 
-def filter_thresholds(data, t_score, t_matches):
+def get_top_k_matches(data: DataFrame, k: int) -> DataFrame:
+    return data.groupby(level=0, group_keys=False).apply(DataFrame.nlargest, n=k, columns=['score'])
+
+
+def filter_thresholds(data: DataFrame, t_score: float, t_matches: float) -> DataFrame:
     filtered = data[data['score'] > t_score]
     filtered = filtered[filtered['matches'] > t_matches]
     return filtered
 
-def load_data(scores_filename, matches_filename):
-    matches = read_csv(matches_filename, sep=';', index_col=0)
-    scores = read_csv(scores_filename, sep=';', index_col=0)
+
+def load_data(scores_filename: str, matches_filename: str) -> DataFrame:
+    matches = read_csv(matches_filename, sep='\t', index_col=0)
+    scores = read_csv(scores_filename, sep='\t', index_col=0)
 
     scores_long = create_long_table(scores, 'score')
     matches_long = create_long_table(matches, 'matches')
-    
-    combined = join_df(matches_long, scores_long, on = ['compound'], how = 'inner')
+
+    combined = join_df(matches_long, scores_long, on=['compound'], how='inner')
     return combined
+
+
+@click.group()
+@click.option('--sf', 'scores_filename', type=click.Path(exists=True), required=True)
+@click.option('--mf', 'matches_filename', type=click.Path(exists=True), required=True)
+@click.option('--o', 'output_filename', type=click.Path(writable=True), required=True)
+@click.pass_context
+def cli(ctx, scores_filename, matches_filename, output_filename):
+    ctx.ensure_object(dict)
+    ctx.obj['data'] = load_data(scores_filename, matches_filename)
+    pass
+
+
+@cli.command()
+@click.option('--st', 'scores_threshold', type=float, required=True)
+@click.option('--mt', 'matches_threshold', type=float, required=True)
+@click.pass_context
+def get_thresholded_data(ctx, scores_threshold, matches_threshold):
+    result = filter_thresholds(ctx.obj['data'], scores_threshold, matches_threshold)
+    return result
+
+
+@cli.command()
+@click.option('--k', 'k', type=int, required=True)
+@click.pass_context
+def get_top_k_data(ctx, k):
+    result = get_top_k_matches(ctx.obj['data'], k)
+    return result
+
+
+@cli.resultcallback()
+def write_output(result, scores_filename, matches_filename, output_filename):
+    result.to_csv(output_filename, sep="\t")
+
+
+if __name__ == '__main__':
+    cli(obj={})
