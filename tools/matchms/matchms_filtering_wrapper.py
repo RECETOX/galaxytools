5a4cf592f7d83bb277933df35b7453baefe6fa65
Matej Trojak <trojak@mail.muni.cz>
2022-01-17 14:12:23 +0100

    Raise exception if no filter is provided

--- tools/matchms/matchms_filtering_wrapper.py
+++ tools/matchms/matchms_filtering_wrapper.py
@@ -29,6 +29,13 @@ def main(argv):
     parser.add_argument("--to_mz", type=float, help="Upper bound for m/z  filter")
     args = parser.parse_args()
 
+    if not args.normalise_intensities or \
+            args.default_filters or \
+            args.clean_metadata or \
+            args.relative_intensity or \
+            args.mz_range:
+        raise ValueError('No filter selected.')
+
     if args.spectra_format == 'msp':
         spectra = list(load_from_msp(args.spectra))
     elif args.queries_format == 'mgf':
