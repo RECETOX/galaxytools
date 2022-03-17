d5bf2b606f3add3aa70353fdb874fc84912caa9f
wverastegui <wudmir@gmail.com>
2022-03-17 20:31:57 +0100

    correct py script

--- tools/msp_split/splitMSP.py
+++ tools/msp_split/splitMSP.py
@@ -1,16 +1,21 @@
-import os 
-import argparse
+import argparse 
+import os
+
+from matchms.exporting import save_as_msp 
 from matchms.importing import load_from_msp
-from matchms.exporting import save_as_msp   
+  
 
 def read_spectra(filename):
     return list(load_from_msp(filename, False))
 
+
 def get_spectra_names(spectra):
     return [x.get("compound_name") for x in spectra]
 
+
 def make_outdir(outdir):
     return os.mkdir(outdir) 
+
 
 def write_spectra(filename, outdir):
     spectra = read_spectra(filename)
@@ -20,9 +25,11 @@ def write_spectra(filename, outdir):
         outpath = os.path.join(outdir, outfile)
         save_as_msp(spectra[i],outpath)
 
+
 def split_spectra(filename,outdir):
     make_outdir(outdir)
     return write_spectra(filename, outdir)
+
 
 listarg = argparse.ArgumentParser()
 listarg.add_argument('--filename', type=str) 
@@ -30,6 +37,7 @@ listarg.add_argument('--outdir', type=str)
 args=listarg.parse_args()
 outdir = args.outdir
 filename = args.filename
+
 
 if __name__=="__main__":
     split_spectra(filename,outdir)
