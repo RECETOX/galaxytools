407e149b16b42881144430dd5e0812cfd1ca4976
wverastegui <wudmir@gmail.com>
2022-03-18 14:20:16 +0100

    lint

--- tools/msp_split/splitMSP.py
+++ tools/msp_split/splitMSP.py
@@ -1,9 +1,9 @@
-import argparse 
+import argparse
 import os
 
-from matchms.exporting import save_as_msp 
+from matchms.exporting import save_as_msp
 from matchms.importing import load_from_msp
-  
+
 
 def read_spectra(filename):
     return list(load_from_msp(filename, False))
@@ -14,34 +14,32 @@ def get_spectra_names(spectra):
 
 
 def make_outdir(outdir):
-    return os.mkdir(outdir) 
+    return os.mkdir(outdir)
 
 
 def write_spectra(filename, outdir):
     spectra = read_spectra(filename)
     names = get_spectra_names(spectra)
     for i in range(len(spectra)):
-        outfile = str(names[i])+".msp"
+        outfile = str(names[i]) + ".msp"
         outpath = os.path.join(outdir, outfile)
-        save_as_msp(spectra[i],outpath)
+        save_as_msp(spectra[i], outpath)
 
 
-def split_spectra(filename,outdir):
+def split_spectra(filename, outdir):
     make_outdir(outdir)
     return write_spectra(filename, outdir)
 
 
 listarg = argparse.ArgumentParser()
-listarg.add_argument('--filename', type=str) 
-listarg.add_argument('--outdir', type=str) 
-args=listarg.parse_args()
+listarg.add_argument('--filename', type=str)
+listarg.add_argument('--outdir', type=str)
+args = listarg.parse_args()
 outdir = args.outdir
 filename = args.filename
 
 
-if __name__=="__main__":
-    split_spectra(filename,outdir)
+if __name__ == "__main__":
+    split_spectra(filename, outdir)
 else:
     print('Do nothing')
-
-
