04f75d6165a5e4616ccce2c6026d88ce9cabc088
Zargham Ahmad <zargham.ahmad12@gmail.com>
2022-09-15 12:12:01 +0200

    removed quotes in SpecAbundance output csv

--- tools/ramclustr/ramclustr_wrapper.R
+++ tools/ramclustr/ramclustr_wrapper.R
@@ -4,7 +4,7 @@ store_output <- function(
     output_spec_abundance,
     msp_file) {
     RAMClustR::write.msp(ramclustr_obj, one.file = output_merge_msp)
-    write.csv(ramclustr_obj$SpecAbund, file = output_spec_abundance, row.names = TRUE)
+    write.csv(ramclustr_obj$SpecAbund, file = output_spec_abundance, row.names = TRUE, quote=FALSE)
 
     if (!is.null(msp_file)) {
         exp_name <- ramclustr_obj$ExpDes[[1]][which(row.names(ramclustr_obj$ExpDes[[1]]) == "Experiment"), 1]
