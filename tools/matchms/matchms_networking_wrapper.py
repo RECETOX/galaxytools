52f1103fdc9ccfbfaf97f69917dae3a6e076725f
maximskorik <maximskoryk@gmail.com>
2022-10-13 12:37:27 +0200

    fix python wrapper

--- tools/matchms/matchms_networking_wrapper.py
+++ tools/matchms/matchms_networking_wrapper.py
@@ -2,19 +2,20 @@ import argparse
 import sys
 
 from matchms.importing import scores_from_json
-from matchms.similarity import SimilarityNetwork
+from matchms.networking import SimilarityNetwork
 
 
 def main(argv):
     parser = argparse.ArgumentParser(description="Create network-graph from similarity scores.")
-    parser.add_argument("scores", type=str, help="Path to matchMS similarity-scores .json file.")
-    parser.add_argument("graph_format", type="str", help="Format of the output similarity network.")
-    parser.add_argument("identifier", type=str, help="Unique metadata identifier of each spectrum from which scores are computed.")
-    parser.add_argument("top_n", type=int, help="Number of highest-score edges to keep.")
-    parser.add_argument("max_links", type=int, help="Maximum number of links to add per node.")
-    parser.add_argument("score_cutoff", type=float, help="Minimum similarity score value to link two spectra.")
-    parser.add_argument("link_method", type=str, help="Method for selecting top N edges for each node.")
-    parser.add_argument("keep_unconnected_nodes", type=bool, help="Keep unconnected nodes in the network.")
+    parser.add_argument("--scores", type=str, help="Path to matchMS similarity-scores .json file.")
+    parser.add_argument("--graph_format", type=str, help="Format of the output similarity network.")
+    parser.add_argument("--identifier", type=str, help="Unique metadata identifier of each spectrum from which scores are computed.")
+    parser.add_argument("--top_n", type=int, help="Number of highest-score edges to keep.")
+    parser.add_argument("--max_links", type=int, help="Maximum number of links to add per node.")
+    parser.add_argument("--score_cutoff", type=float, help="Minimum similarity score value to link two spectra.")
+    parser.add_argument("--link_method", type=str, help="Method for selecting top N edges for each node.")
+    parser.add_argument("--keep_unconnected_nodes", type=bool, help="Keep unconnected nodes in the network.")
+    parser.add_argument("--output_filename", type=str, help="Path where to store the output similarity network.")
     args = parser.parse_args()
 
     scores = scores_from_json(args.scores)
@@ -24,7 +25,7 @@ def main(argv):
         max_links=args.max_links,
         score_cutoff=args.score_cutoff,
         link_method=args.link_method,
-        keep_unconected_nodes=args.keep_unconnected_nodes)
+        keep_unconnected_nodes=args.keep_unconnected_nodes)
 
     network.create_network(scores)
     network.export_to_file(filename=args.output_filename, graph_format=args.graph_format)
@@ -34,3 +35,4 @@ def main(argv):
 
 if __name__ == "__main__":
     main(argv=sys.argv[1:])
+    pass
