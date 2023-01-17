fc7755a1e1d84f2cef11d2fa44c97f5efe30966b
maximskorik <maximskoryk@gmail.com>
2023-01-17 15:29:44 +0100

    use pymzml to get ids from mzMLs

---
+++ tools/recetox_aplcms/mzml_id_getter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+
+from pymzml.run import Reader
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Get the ID from an mzML file.')
+    parser.add_argument('mzml_file', help='Path to an mzML file to get the ID from.')
+    args = parser.parse_args()
+
+    mzml = Reader(args.mzml_file)
+    id = mzml.info['run_id']
+
+    if id is None:
+        sys.stderr.write('ERROR: The mzML file has no run ID.')
+        sys.exit(1)
+
+    sys.stdout.write(id)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
