36f4ff86461f6ea937e595dd2afa8cc1d4a4b3b9
Matej Trojak <trojak@mail.muni.cz>
2023-06-19 14:47:50 +0200

    Currently support only parquet tables

--- tools/recetox_xmsannotator/utils.R
+++ tools/recetox_xmsannotator/utils.R
@@ -1,4 +1,26 @@
 library(recetox.xmsannotator)
+library(dplyr)
+
+load_adduct_table <- function(filename) {
+    if (filename == "None") {
+        return(NULL)
+    }
+    return(load_adduct_table_parquet(filename))
+}
+
+load_compound_table <- function(filename) {
+    if (filename == "None") {
+        return(NULL)
+    }
+    return(load_compound_table_parquet(filename))
+}
+
+load_table <- function(filename) {
+    if (filename == "None") {
+        return(NULL)
+    }
+    return(arrow::read_parquet(filename))
+}
 
 create_filter_by_adducts <- function(comma_separated_values) {
     if (comma_separated_values == "None") {
@@ -9,10 +31,10 @@ create_filter_by_adducts <- function(comma_separated_values) {
 }
 
 create_peak_table <- function(metadata_table, intensity_table) {
-    metadata <- arrow::read_parquet(metadata_table) %>% select(id, mz, rt)
-    intensity <- arrow::read_parquet(intensity_table)
-    
-    peak_table <- inner_join(metadata, intensity, by = "id")
+    metadata_table <- load_table(metadata_table) %>% select(id, mz, rt)
+    intensity_table <- load_table(intensity_table)
+    peak_table <- inner_join(metadata_table, intensity_table, by = "id")
     peak_table <- rename(peak_table, peak = id)
+    peak_table$peak <- as.integer(peak_table$peak)
     return(peak_table)
 }
