e67207cdfed93872c443e4e24e8fdef560cfb432
Matej Trojak <trojak@mail.muni.cz>
2023-06-19 11:08:03 +0200

    Update input to reflect changes in aplcms

--- tools/recetox_xmsannotator/utils.R
+++ tools/recetox_xmsannotator/utils.R
@@ -1,20 +1,18 @@
 library(recetox.xmsannotator)
 
-read_peak_table <- function(peak_table) {
-    peak_table <- arrow::read_parquet(peak_table)
-
-    if ("peak" %in% colnames(peak_table)) {
-        if (!is.integer(peak_table$peak)) {
-            peak_table$peak <- as.integer(peak_table$peak)
-        }
-    }
-    return(peak_table)
-}
-
 create_filter_by_adducts <- function(comma_separated_values) {
     if (comma_separated_values == "None") {
         return(NA)
     }
     filter_by <- strsplit(trimws(comma_separated_values), ",")[[1]]
     return(filter_by)
+}
+
+create_peak_table <- function(metadata_table, intensity_table) {
+    metadata <- arrow::read_parquet(metadata_table) %>% select(id, mz, rt)
+    intensity <- arrow::read_parquet(intensity_table)
+    
+    peak_table <- inner_join(metadata, intensity, by = "id")
+    peak_table <- rename(peak_table, peak = id)
+    return(peak_table)
 }
