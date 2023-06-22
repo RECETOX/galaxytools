e9b4fc4d65cda15b701fd9fd8b42ff3ffbe3d5b8
Matej Trojak <trojak@mail.muni.cz>
2023-06-22 11:14:08 +0200

    Add support for CSV file

--- tools/recetox_xmsannotator/utils.R
+++ tools/recetox_xmsannotator/utils.R
@@ -1,25 +1,23 @@
 library(recetox.xmsannotator)
 library(dplyr)
 
-load_adduct_table <- function(filename) {
+load_table <- function(filename, filetype) {
     if (filename == "None") {
         return(NULL)
     }
-    return(load_adduct_table_parquet(filename))
+    if (filetype == "csv") {
+        return(as.data.frame(read.csv(filename)))
+    } else {
+        return(as.data.frame(arrow::read_parquet(filename)))
+    }
 }
 
-load_compound_table <- function(filename) {
-    if (filename == "None") {
-        return(NULL)
+save_table <- function(table, filename, filetype) {
+    if (filetype == "csv") {
+        write.csv(table, filename, row.names = FALSE)
+    } else {
+        arrow::write_parquet(table, filename)
     }
-    return(load_compound_table_parquet(filename))
-}
-
-load_table <- function(filename) {
-    if (filename == "None") {
-        return(NULL)
-    }
-    return(as.data.frame(arrow::read_parquet(filename)))
 }
 
 create_filter_by_adducts <- function(comma_separated_values) {
@@ -31,8 +29,7 @@ create_filter_by_adducts <- function(comma_separated_values) {
 }
 
 create_peak_table <- function(metadata_table, intensity_table) {
-    metadata_table <- load_table(metadata_table) %>% select(id, mz, rt)
-    intensity_table <- load_table(intensity_table)
+    metadata_table <- select(metadata_table, id, mz, rt)
     peak_table <- inner_join(metadata_table, intensity_table, by = "id")
     peak_table <- rename(peak_table, peak = id)
     peak_table$peak <- as.integer(peak_table$peak)
