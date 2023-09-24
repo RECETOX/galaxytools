e9e6239ac42648d691a270932fdb722460e3f55f
Zargham Ahmad <zargham.ahmad12@gmail.com>
2023-09-24 20:21:19 +0200

    fixed R linting

--- tools/ramclustr/ramclustr_wrapper.R
+++ tools/ramclustr/ramclustr_wrapper.R
@@ -1,139 +1,138 @@
-store_output <- function(ramclustr_obj,
-                         output_merge_msp,
-                         output_spec_abundance,
-                         msp_file) {
-    RAMClustR::write.msp(ramclustr_obj, one.file = output_merge_msp)
-    write.csv(ramclustr_obj$SpecAbund,
-        file = output_spec_abundance,
-        row.names = TRUE, quote = FALSE
-    )
+store_output < - function(ramclustr_obj,
+                          output_merge_msp,
+                          output_spec_abundance,
+                          msp_file) {
+  RAMClustR::write.msp(ramclustr_obj, one.file = output_merge_msp)
+  write.csv(ramclustr_obj$SpecAbund,
+    file = output_spec_abundance,
+    row.names = TRUE, quote = FALSE
+  )
 
-    if (!is.null(msp_file)) {
-        exp_name <- ramclustr_obj$ExpDes[[1]][which(
-            row.names(ramclustr_obj$ExpDes[[1]]) == "Experiment"
-        ), 1]
-        filename <- paste("spectra/", exp_name, ".msp", sep = "")
-        file.copy(from = filename, to = msp_file, overwrite = TRUE)
-    }
+  if (!is.null(msp_file)) {
+    exp_name < -ramclustr_obj$ExpDes[[1]][which(
+      row.names(ramclustr_obj$ExpDes[[1]]) == "Experiment"
+    ), 1]
+    filename < -paste("spectra/", exp_name, ".msp", sep = "")
+    file.copy(from = filename, to = msp_file, overwrite = TRUE)
+  }
 }
 
-load_experiment_definition <- function(filename) {
-    experiment <- RAMClustR::defineExperiment(csv = filename)
-    return(experiment)
+load_experiment_definition < - function(filename) {
+  experiment < -RAMClustR::defineExperiment(csv = filename)
+  return(experiment)
 }
 
-read_metadata <- function(filename) {
-    data <- read.csv(filename, header = TRUE, stringsAsFactors = FALSE)
+read_metadata < - function(filename) {
+  data < -read.csv(filename, header = TRUE, stringsAsFactors = FALSE)
 
-    if (!"qc" %in% colnames(data)) {
-        if ("sampleType" %in% colnames(data)) {
-            data$qc <- ifelse(data$sampleType == "qc", TRUE, FALSE)
-        }
+  if (!"qc" % in % colnames(data)) {
+    if ("sampleType" % in % colnames(data)) {
+      data$qc < -ifelse(data$sampleType == "qc", TRUE, FALSE)
     }
+  }
 
-    if (!"order" %in% colnames(data)) {
-        if ("injectionOrder" %in% colnames(data)) {
-            names(data)[names(data) == "injectionOrder"] <- "order"
-        }
+  if (!"order" % in % colnames(data)) {
+    if ("injectionOrder" % in % colnames(data)) {
+      names(data)[names(data) == "injectionOrder"] < -"order"
     }
+  }
 
-    return(data)
+  return(data)
 }
 
-read_ramclustr_aplcms <- function(ms1_featuredefinitions = NULL,
-                                  ms1_featurevalues = NULL,
-                                  df_phenodata = NULL,
-                                  phenodata_ext = NULL,
-                                  exp_des = NULL,
-                                  st = NULL,
-                                  ensure_no_na = TRUE) {
-    ms1_featuredefinitions <- arrow::read_parquet(ms1_featuredefinitions)
-    ms1_featurevalues <- arrow::read_parquet(ms1_featurevalues)
+read_ramclustr_aplcms < - function(ms1_featuredefinitions = NULL,
+                                   ms1_featurevalues = NULL,
+                                   df_phenodata = NULL,
+                                   phenodata_ext = NULL,
+                                   exp_des = NULL,
+                                   st = NULL,
+                                   ensure_no_na = TRUE) {
+  ms1_featuredefinitions < -arrow::read_parquet(ms1_featuredefinitions)
+  ms1_featurevalues < -arrow::read_parquet(ms1_featurevalues)
 
-    if (!is.null(df_phenodata)) {
-        if (phenodata_ext == "csv") {
-            df_phenodata <- read.csv(
-                file = df_phenodata,
-                header = TRUE, check.names = FALSE
-            )
-        } else {
-            df_phenodata <- read.csv(
-                file = df_phenodata,
-                header = TRUE, check.names = FALSE, sep = "\t"
-            )
-        }
-    }
-    if (!is.null(exp_des)) {
-        exp_des <- load_experiment_definition(exp_des)
-    }
-
-    feature_values <- ms1_featurevalues[-1]
-    feature_values <- t(feature_values)
-    colnames(feature_values) <- ms1_featurevalues[[1]]
-
-    feature_definitions <- data.frame(ms1_featuredefinitions)
-
-    ramclustr_obj <- RAMClustR::rc.get.df.data(
-        ms1_featureDefinitions = feature_definitions,
-        ms1_featureValues = feature_values,
-        phenoData = df_phenodata,
-        ExpDes = exp_des,
-        st = st,
-        ensure.no.na = ensure_no_na
-    )
-    return(ramclustr_obj)
-}
-
-apply_normalisation <- function(ramclustr_obj = NULL,
-                                normalize_method,
-                                metadata_file = NULL,
-                                qc_inj_range,
-                                p_cut,
-                                rsq_cut,
-                                p_adjust) {
-    batch <- NULL
-    order <- NULL
-    qc <- NULL
-
-    if (normalize_method == "TIC") {
-        ramclustr_obj <- RAMClustR::rc.feature.normalize.tic(
-            ramclustObj =
-                ramclustr_obj
-        )
-    } else if (normalize_method == "quantile") {
-        ramclustr_obj <- RAMClustR::rc.feature.normalize.quantile(ramclustr_obj)
-    } else if (normalize_method == "batch.qc") {
-        if (!(is.null(metadata_file) || metadata_file == "None")) {
-            metadata <- read_metadata(metadata_file)
-            batch <- metadata$batch
-            order <- metadata$order
-            qc <- metadata$qc
-        }
-
-        ramclustr_obj <- RAMClustR::rc.feature.normalize.batch.qc(
-            order = order,
-            batch = batch,
-            qc = qc,
-            ramclustObj = ramclustr_obj,
-            qc.inj.range = qc_inj_range
-        )
+  if (!is.null(df_phenodata)) {
+    if (phenodata_ext == "csv") {
+      df_phenodata < -read.csv(
+        file = df_phenodata,
+        header = TRUE, check.names = FALSE
+      )
     } else {
-        if (!(is.null(metadata_file) || metadata_file == "None")) {
-            metadata <- read_metadata(metadata_file)
-            batch <- metadata$batch
-            order <- metadata$order
-            qc <- metadata$qc
-        }
-
-        ramclustr_obj <- RAMClustR::rc.feature.normalize.qc(
-            order = order,
-            batch = batch,
-            qc = qc,
-            ramclustObj = ramclustr_obj,
-            p.cut = p_cut,
-            rsq.cut = rsq_cut,
-            p.adjust = p_adjust
-        )
+      df_phenodata < -read.csv(
+        file = df_phenodata,
+        header = TRUE, check.names = FALSE, sep = "\t"
+      )
     }
-    return(ramclustr_obj)
+  }
+  if (!is.null(exp_des)) {
+    exp_des < -load_experiment_definition(exp_des)
+  }
+
+  feature_values < -ms1_featurevalues[-1]
+  feature_values < -t(feature_values)
+  colnames(feature_values) < -ms1_featurevalues[[1]]
+
+  feature_definitions < -data.frame(ms1_featuredefinitions)
+
+  ramclustr_obj < -RAMClustR::rc.get.df.data(
+    ms1_featureDefinitions = feature_definitions,
+    ms1_featureValues = feature_values,
+    phenoData = df_phenodata,
+    ExpDes = exp_des,
+    st = st,
+    ensure.no.na = ensure_no_na
+  )
+  return(ramclustr_obj)
+}
+
+apply_normalisation < - function(ramclustr_obj = NULL,
+                                 normalize_method,
+                                 metadata_file = NULL,
+                                 qc_inj_range,
+                                 p_cut,
+                                 rsq_cut,
+                                 p_adjust) {
+  batch < -NULL
+  order < -NULL
+  qc < -NULL
+
+  if (normalize_method == "TIC") {
+    ramclustr_obj < -RAMClustR::rc.feature.normalize.tic(
+      ramclustObj = ramclustr_obj
+    )
+  } else if (normalize_method == "quantile") {
+    ramclustr_obj < -RAMClustR::rc.feature.normalize.quantile(ramclustr_obj)
+  } else if (normalize_method == "batch.qc") {
+    if (!(is.null(metadata_file) || metadata_file == "None")) {
+      metadata < -read_metadata(metadata_file)
+      batch < -metadata$batch
+      order < -metadata$order
+      qc < -metadata$qc
+    }
+
+    ramclustr_obj < -RAMClustR::rc.feature.normalize.batch.qc(
+      order = order,
+      batch = batch,
+      qc = qc,
+      ramclustObj = ramclustr_obj,
+      qc.inj.range = qc_inj_range
+    )
+  } else {
+    if (!(is.null(metadata_file) || metadata_file == "None")) {
+      metadata < -read_metadata(metadata_file)
+      batch < -metadata$batch
+      order < -metadata$order
+      qc < -metadata$qc
+    }
+
+    ramclustr_obj < -RAMClustR::rc.feature.normalize.qc(
+      order = order,
+      batch = batch,
+      qc = qc,
+      ramclustObj = ramclustr_obj,
+      p.cut = p_cut,
+      rsq.cut = rsq_cut,
+      p.adjust = p_adjust
+    )
+  }
+  return(ramclustr_obj)
 }
--- tools/recetox_aplcms/utils.R
+++ tools/recetox_aplcms/utils.R
@@ -1,131 +1,131 @@
 library(recetox.aplcms)
 
-get_env_sample_name <- function() {
-    sample_name <- Sys.getenv("SAMPLE_NAME", unset = NA)
-    if (nchar(sample_name) == 0) {
-        sample_name <- NA
-    }
-    if (is.na(sample_name)) {
-        message("The mzML file does not contain run ID.")
-    }
+get_env_sample_name < - function() {
+  sample_name < -Sys.getenv("SAMPLE_NAME", unset = NA)
+  if (nchar(sample_name) == 0) {
+    sample_name < -NA
+  }
+  if (is.na(sample_name)) {
+    message("The mzML file does not contain run ID.")
+  }
+  return(sample_name)
+}
+
+save_sample_name < - function(df, sample_name) {
+  attr(df, "sample_name") < -sample_name
+  return(df)
+}
+
+restore_sample_name < - function(df) {
+  return(df$sample_id[1])
+}
+
+load_sample_name < - function(df) {
+  sample_name < -attr(df, "sample_name")
+  if (is.null(sample_name)) {
+    return(NA)
+  } else {
     return(sample_name)
+  }
 }
 
-save_sample_name <- function(df, sample_name) {
-    attr(df, "sample_name") <- sample_name
-    return(df)
+save_data_as_parquet_file < - function(data, filename) {
+  arrow::write_parquet(data, filename)
 }
 
-restore_sample_name <- function(df) {
-    return(df$sample_id[1])
+load_data_from_parquet_file < - function(filename) {
+  return(arrow::read_parquet(filename))
 }
 
-load_sample_name <- function(df) {
-    sample_name <- attr(df, "sample_name")
-    if (is.null(sample_name)) {
-        return(NA)
-    } else {
-        return(sample_name)
-    }
+load_parquet_collection < - function(files) {
+  features < -lapply(files, arrow::read_parquet)
+  features < -lapply(features, tibble::as_tibble)
+  return(features)
 }
 
-save_data_as_parquet_file <- function(data, filename) {
-    arrow::write_parquet(data, filename)
+save_parquet_collection < - function(feature_tables, sample_names, subdir) {
+  dir.create(subdir)
+  for (i in seq_len(length(feature_tables))) {
+    filename < -file.path(subdir, paste0(sample_names[i], ".parquet"))
+    feature_table < -as.data.frame(feature_tables[[i]])
+    feature_table < -save_sample_name(feature_table, sample_names[i])
+    arrow::write_parquet(feature_table, filename)
+  }
 }
 
-load_data_from_parquet_file <- function(filename) {
-    return(arrow::read_parquet(filename))
+sort_by_sample_name < - function(tables, sample_names) {
+  return(tables[order(sample_names)])
 }
 
-load_parquet_collection <- function(files) {
-    features <- lapply(files, arrow::read_parquet)
-    features <- lapply(features, tibble::as_tibble)
-    return(features)
+save_tolerances < - function(table, tol_file) {
+  mz_tolerance < -c(table$mz_tol_relative)
+  rt_tolerance < -c(table$rt_tol_relative)
+  arrow::write_parquet(data.frame(mz_tolerance, rt_tolerance), tol_file)
 }
 
-save_parquet_collection <- function(feature_tables, sample_names, subdir) {
-    dir.create(subdir)
-    for (i in seq_len(length(feature_tables))) {
-      filename <- file.path(subdir, paste0(sample_names[i], ".parquet"))
-      feature_table <- as.data.frame(feature_tables[[i]])
-      feature_table <- save_sample_name(feature_table, sample_names[i])
-      arrow::write_parquet(feature_table, filename)
-    }
+save_aligned_features < - function(aligned_features, metadata_file, rt_file, intensity_file) {
+  save_data_as_parquet_file(aligned_features$metadata, metadata_file)
+  save_data_as_parquet_file(aligned_features$rt, rt_file)
+  save_data_as_parquet_file(aligned_features$intensity, intensity_file)
 }
 
-sort_by_sample_name <- function(tables, sample_names) {
-    return(tables[order(sample_names)])
+select_table_with_sample_name < - function(tables, sample_name) {
+  sample_names < -lapply(tables, load_sample_name)
+  index < -which(sample_names == sample_name)
+  if (length(index) > 0) {
+    return(tables[[index]])
+  } else {
+    stop(sprintf("Mismatch - sample name '%s' not present in %s",
+                 sample_name, paste(sample_names, collapse = ", ")))
+  }
 }
 
-save_tolerances <- function(table, tol_file) {
-    mz_tolerance <- c(table$mz_tol_relative)
-    rt_tolerance <- c(table$rt_tol_relative)
-    arrow::write_parquet(data.frame(mz_tolerance, rt_tolerance), tol_file)
+select_adjusted < - function(recovered_features) {
+  return(recovered_features$adjusted_features)
 }
 
-save_aligned_features <- function(aligned_features, metadata_file, rt_file, intensity_file) {
-    save_data_as_parquet_file(aligned_features$metadata, metadata_file)
-    save_data_as_parquet_file(aligned_features$rt, rt_file)
-    save_data_as_parquet_file(aligned_features$intensity, intensity_file)
-}
-
-select_table_with_sample_name <- function(tables, sample_name) {
-    sample_names <- lapply(tables, load_sample_name)
-    index <- which(sample_names == sample_name)
-    if (length(index) > 0) {
-        return(tables[[index]])
-    } else {
-        stop(sprintf("Mismatch - sample name '%s' not present in %s",
-                     sample_name, paste(sample_names, collapse = ", ")))
-    }
-}
-
-select_adjusted <- function(recovered_features) {
-    return(recovered_features$adjusted_features)
-}
-
-known_table_columns <- function() {
+known_table_columns < - function() {
   c("chemical_formula", "HMDB_ID", "KEGG_compound_ID", "mass", "ion.type",
     "m.z", "Number_profiles_processed", "Percent_found", "mz_min", "mz_max",
     "RT_mean", "RT_sd", "RT_min", "RT_max", "int_mean(log)", "int_sd(log)",
     "int_min(log)", "int_max(log)")
 }
 
-save_known_table <- function(table, filename) {
-  columns <- known_table_columns()
+save_known_table < - function(table, filename) {
+  columns < -known_table_columns()
   arrow::write_parquet(table$known_table[columns], filename)
 }
 
-read_known_table <- function(filename) {
+read_known_table < - function(filename) {
   arrow::read_parquet(filename, col_select = known_table_columns())
 }
 
-save_pairing <- function(table, filename) {
-  df <- table$pairing %>% as_tibble() %>% setNames(c("new", "old"))
+save_pairing < - function(table, filename) {
+  df < -table$pairing % > % as_tibble() % > % setNames(c("new", "old"))
   arrow::write_parquet(df, filename)
 }
 
-join_tables_to_list <- function(metadata, rt_table, intensity_table) {
-  features <- new("list")
-  features$metadata <- metadata
-  features$intensity <- intensity_table
-  features$rt <- rt_table
+join_tables_to_list < - function(metadata, rt_table, intensity_table) {
+  features < -new("list")
+  features$metadata < -metadata
+  features$intensity < -intensity_table
+  features$rt < -rt_table
   return(features)
 }
 
-validate_sample_names <- function(sample_names) {
-    if ((any(is.na(sample_names))) || (length(unique(sample_names)) != length(sample_names))) {
-        stop(sprintf("Sample names absent or not unique - provided sample names: %s",
-                     paste(sample_names, collapse = ", ")))
-    }
+validate_sample_names < - function(sample_names) {
+  if ((any(is.na(sample_names))) || (length(unique(sample_names)) != length(sample_names))) {
+    stop(sprintf("Sample names absent or not unique - provided sample names: %s",
+                 paste(sample_names, collapse = ", ")))
+  }
 }
 
-determine_sigma_ratios <- function(sigma_ratio_lim_min = NA, sigma_ratio_lim_max = NA) {
-    if (is.na(sigma_ratio_lim_min)) {
-        sigma_ratio_lim_min <- 0
-    }
-    if (is.na(sigma_ratio_lim_max)) {
-        sigma_ratio_lim_max <- Inf
-    }
-    return(c(sigma_ratio_lim_min, sigma_ratio_lim_max))
+determine_sigma_ratios < - function(sigma_ratio_lim_min = NA, sigma_ratio_lim_max = NA) {
+  if (is.na(sigma_ratio_lim_min)) {
+    sigma_ratio_lim_min < -0
+  }
+  if (is.na(sigma_ratio_lim_max)) {
+    sigma_ratio_lim_max < -Inf
+  }
+  return(c(sigma_ratio_lim_min, sigma_ratio_lim_max))
 }
