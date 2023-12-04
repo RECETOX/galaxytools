3c28584a5ec72382fb3a251f6558490b02ee9837
hechth <helge.hecht@recetox.muni.cz>
2023-12-04 19:38:00 +0100

    lint

--- tools/ipapy2/ipapy2_clustering.py
+++ tools/ipapy2/ipapy2_clustering.py
@@ -7,9 +7,10 @@ from ipaPy2 import ipa
 @click.option('--i', 'input_filename', type=click.Path(exists=True), required=True)
 @click.option('--o', 'output_filename', type=click.Path(writable=True), required=True)
 def cli(input_filename, output_filename):
-    intensity_table=pd.read_csv(input_filename)
-    result=ipa.clusterFeatures(intensity_table)
+    intensity_table = pd.read_csv(input_filename)
+    result = ipa.clusterFeatures(intensity_table)
     result.to_csv(output_filename, index=False)
+
 
 if __name__ == '__main__':
     cli()
