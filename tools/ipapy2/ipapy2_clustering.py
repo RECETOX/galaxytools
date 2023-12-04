a338bbc09c8c5b9d24f6ae4a9d34e645dbe1858e
hechth <helge.hecht@recetox.muni.cz>
2023-12-04 16:55:05 +0100

    started ipapy2 clustering tool

---
+++ tools/ipapy2/ipapy2_clustering.py
@@ -0,0 +1,15 @@
+import click
+import pandas as pd
+from ipaPy2 import ipa
+
+
+@click.group(invoke_without_command=True)
+@click.option('--i', 'input_filename', type=click.Path(exists=True), required=True)
+@click.option('--o', 'output_filename', type=click.Path(writable=True), required=True)
+def cli(input_filename, output_filename):
+    intensity_table=pd.read_csv(input_filename)
+    result=ipa.clusterFeatures(intensity_table)
+    result.to_csv(output_filename, index=False)
+
+if __name__ == '__main__':
+    cli()
