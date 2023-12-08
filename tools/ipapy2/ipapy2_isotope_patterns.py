a7f5ff8703cab739b81c761fc4eccffe4c29b0c5
ChangLiuuczlcl8 <51643117+ChangLiuuczlcl8@users.noreply.github.com>
2023-12-08 19:53:17 +0000

    Add files via upload

--- tools/ipapy2/ipapy2_isotope_patterns.py
+++ tools/ipapy2/ipapy2_isotope_patterns.py
@@ -1,17 +1,22 @@
-import click
+import argparse
+import sys
 import pandas as pd
 from ipaPy2 import ipa
 
 
-@click.group(invoke_without_command=True)
-@click.option('input_filename', type=click.Path(exists=True), required=True)
-@click.option('--ionisation', type=click.Path(exists=True), required=True)
-@click.option('output_filename', type=click.Path(writable=True), required=True)
-def cli(input_filename, ionisation, output_filename):
-    ipa_dataframe = pd.read_csv(input_filename)
-    ipa.map_isotope_patterns(ipa_dataframe,ionisation=ionisation)
-    ipa_dataframe.to_csv(output_filename, index=False)
+def main(argv):
+    parser = argparse.ArgumentParser(description="mapping the isotope pattern within the dataset.")
+    parser.add_argument("input_filename", type=str, help="a dataframe of clustered features.")
+    parser.add_argument("--isotope_diff", type=float, help="Difference between isotopes of charge 1.")
+    parser.add_argument("--ppm", type=float, help="Default value 100. Maximum ppm value allowed between 2 isotopes.")
+    parser.add_argument("--ionisation", type=int, help="ionisation. +1 or -1.")
+    parser.add_argument("--isotope_ratio", type=float, defaul=1, help="mininum intensity ratio expressed (Default value 0.5%). Only isotopes with intensity higher than this value/100 of the main isotope are considered.")
+    parser.add_argument("output_filename", type=str, help="a dataframe containing all possible adducts given the database.")
+    args = parser.parse_args()
 
+    ipa_dataframe = pd.read_csv(args.input_filename)
+    ipa.map_isotope_patterns(ipa_dataframe, isoDiff=args.isotope_diff, ppm=args.ppm, ionisation=args.ionisation, MinIsoRatio=args.isotope_ratio)
+    ipa_dataframe.to_csv(args.output_filename, index=False)
 
 if __name__ == '__main__':
-    cli()
+    main(argv=sys.argv[1:])
