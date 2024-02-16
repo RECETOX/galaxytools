bccea41a169c600b54f478432032258e1b85c60c
wverastegui <wudmir.rojas@recetox.muni.cz>
2024-02-16 17:35:18 +0100

    Linting

--- tools/misc/use_theoretical_mz_annotations.py
+++ tools/misc/use_theoretical_mz_annotations.py
@@ -1,5 +1,6 @@
-from typing import Tuple, Iterator
 import argparse
+from typing import Tuple, Iterator
+
 
 def get_peak_values(peak: str) -> Tuple[float, float, str]:
     """ Get the m/z and intensity value from the line containing the peak information. """
@@ -7,15 +8,17 @@ def get_peak_values(peak: str) -> Tuple[float, float, str]:
     mz = float(splitted_line[0].strip())
     intensity = float(splitted_line[1].strip())
     comment = ''
-    if(len(splitted_line) == 3):
+    if (len(splitted_line) == 3):
         comment = splitted_line[2].strip()
     return mz, intensity, comment
+
 
 def get_peak_tuples(rline: str) -> Iterator[str]:
     """ Splits line at ';' and performs additional string cleaning. """
     tokens = filter(None, rline.split(";"))
     peak_pairs = map(lambda x: x.lstrip().rstrip(), tokens)
     return peak_pairs
+
 
 def overwrite_peaks(file: str, output: str, only_contains_annotation: bool = False) -> None:
     """This function overwrites peaks in the input file with annotated peaks.
@@ -30,8 +33,9 @@ def overwrite_peaks(file: str, output: str, only_contains_annotation: bool = Fal
     """
     annotated_msp = []
     annotated_msp_list = []
-    
-    with open(file,'r') as file:
+    peaks = []
+
+    with open(file, 'r') as file:
         while True:
             line = file.readline()
             if not line.strip():
@@ -46,7 +50,7 @@ def overwrite_peaks(file: str, output: str, only_contains_annotation: bool = Fal
                 for i in range(num_peaks):
                     line = file.readline()
                     peak_pairs = get_peak_tuples(line)
-                    
+
                     for peak in peak_pairs:
                         mz, intensity, comment = get_peak_values(peak)
                         if comment != '':
@@ -54,20 +58,21 @@ def overwrite_peaks(file: str, output: str, only_contains_annotation: bool = Fal
                             mz = float(tokens[2].strip().rstrip(','))
                             peak_text = '%s\t%s\t%s\n' % (str(mz), str(intensity), str(comment))
                             peaks.append(peak_text)
-                        
-                        if only_contains_annotation == False and comment == '':
+
+                        if only_contains_annotation is False and comment == '':
                             peak_text = '%s\t%s\n' % (str(mz), str(intensity))
                             peaks.append(peak_text)
-                            
+
                 annotated_msp.append("Num Peaks: %d\n" % len(peaks))
                 for item in peaks:
                     annotated_msp.append(item)
             else:
                 annotated_msp.append(line)
-    
-    with open(output,'w') as file:
+
+    with open(output, 'w') as file:
         for spectra in annotated_msp_list:
             file.writelines(spectra)
+
 
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
