ac09c216e5b616b1ade3d2149a943cc3cb7951e5
hechth <helge.hecht@recetox.muni.cz>
2024-04-22 16:15:32 +0200

    lint

--- tools/isolib/isolib.R
+++ tools/isolib/isolib.R
@@ -13,62 +13,62 @@ main <- function() {
   adducts_to_use <- c(unlist(strsplit(args[2], ',', fixed = TRUE)))
 
   chemforms <- compound_table$formula
-  chemforms <- check_chemform(isotopes, chemforms)[,2]
+  chemforms <- check_chemform(isotopes, chemforms)[, 2]
   
   
   spectra <- data.frame()
   
   for (current in adducts_to_use) {
-      adduct <- adducts[adducts$Name == current,]
-      multiplied_chemforms <- multiform(chemforms, adduct$Mult)
+    adduct <- adducts[adducts$Name == current,]
+    multiplied_chemforms <- multiform(chemforms, adduct$Mult)
   
-      if(adduct$Ion_mode == "negative") {
-        merged_chemforms <- subform(multiplied_chemforms, adduct$Formula_ded)
-      } else {
-        merged_chemforms <- mergeform(multiplied_chemforms, adduct$Formula_add)
-      }
+    if(adduct$Ion_mode == "negative") {
+      merged_chemforms <- subform(multiplied_chemforms, adduct$Formula_ded)
+    } else {
+      merged_chemforms <- mergeform(multiplied_chemforms, adduct$Formula_add)
+    }
   
-      charge_string <- paste0(if(adduct$Charge > 0) "+" else "-", if(abs(adduct$Charge) > 1) abs(adduct$Charge) else "")
-      adduct_string <- paste0("[", adduct$Name, "]", charge_string)
-      precursor_mz <- calculateMass(multiplied_chemforms) + adduct$Mass
+    charge_string <- paste0(if(adduct$Charge > 0) "+" else "-", if(abs(adduct$Charge) > 1) abs(adduct$Charge) else "")
+    adduct_string <- paste0("[", adduct$Name, "]", charge_string)
+    precursor_mz <- calculateMass(multiplied_chemforms) + adduct$Mass
 
-      if (args[4] == TRUE) {
-        names <- paste(compound_table$name, paste0('(', adduct$Name, ')'), sep=' ')
-      } else {
-        names <- compound_table$name
-      }
+    if (args[4] == TRUE) {
+      names <- paste(compound_table$name, paste0('(', adduct$Name, ')'), sep=' ')
+    } else {
+      names <- compound_table$name
+    }
       
-      spectra_df <- data.frame(
-          name=names,
-          adduct=adduct_string,
-          formula=chemforms,
-          charge=adduct$Charge,
-          ionization_mode=adduct$Ion_mode,
-          precursor_mz=precursor_mz,
-          msLevel=as.integer(1)
-      )
+    spectra_df <- data.frame(
+        name=names,
+        adduct=adduct_string,
+        formula=chemforms,
+        charge=adduct$Charge,
+        ionization_mode=adduct$Ion_mode,
+        precursor_mz=precursor_mz,
+        msLevel=as.integer(1)
+    )
 
-      if ('rt' %in% colnames(compound_table)) {
-        spectra_df$retention_time=compound_table$rt
-      }
+    if ('rt' %in% colnames(compound_table)) {
+      spectra_df$retention_time <- compound_table$rt
+    }
  
-      patterns <- enviPat::isopattern(
-          isotopes=isotopes,
-          chemforms=merged_chemforms,
-          charge=adduct$Charge,
-          threshold=as.numeric(args[3]),
-      )
+    patterns <- enviPat::isopattern(
+      isotopes = isotopes,
+      chemforms = merged_chemforms,
+      charge = adduct$Charge,
+      threshold = as.numeric(args[3]),
+    )
   
-      mzs <- list()
-      intensities <- list()
-      for (i in 1:length(patterns)) {
-          mzs <- append(mzs, list(patterns[[i]][,1]))
-          intensities <- append(intensities, list(patterns[[i]][,2]))
-      }
+    mzs <- list()
+    intensities <- list()
+    for (i in 1:length(patterns)) {
+        mzs <- append(mzs, list(patterns[[i]][, 1]))
+        intensities <- append(intensities, list(patterns[[i]][, 2]))
+    }
   
-      spectra_df$mz <- mzs
-      spectra_df$intensity <- intensities
-      spectra <- rbind(spectra, spectra_df)
+    spectra_df$mz <- mzs
+    spectra_df$intensity <- intensities
+    spectra <- rbind(spectra, spectra_df)
   }
   
   sps <- Spectra(spectra)
