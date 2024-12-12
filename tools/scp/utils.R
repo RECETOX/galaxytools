192344535d1c55824eeba459ffb8f5a50f287542
KristinaGomoryova <gomoryova@sci.muni.cz>
2024-12-12 15:29:34 +0100

    function for intermediate tables export

--- tools/scp/utils.R
+++ tools/scp/utils.R
@@ -1,9 +1,6 @@
 library(dplyr)
 library(scp)
 
-# GREP ONLY NAMES WITH _IMP, _NORM, _LOG, _BATCHC; don't report all datasets
-
-
 # Function to export a single assay with metadata
 export_assay_with_metadata <- function(qf, assay_name) {
   # Extract assay data, row metadata, and col metadata
@@ -15,15 +12,16 @@ export_assay_with_metadata <- function(qf, assay_name) {
   export_data <- cbind(RowNames = rownames(assay_data), row_metadata, as.data.frame(assay_data))
   
   # Save the table to a CSV file
-  output_file <- paste0(assay_name, "_export.csv")
+  output_file <- file.path("outputs", paste0(assay_name, "_export.csv"))
   write.csv(export_data, output_file, row.names = FALSE)
 }
 
 # Export all assays
 export_all_assays <- function(qf) {
   # Get the names of all assays
-  assay_names <- names(assays(qf))
-  
+  # assay_names <- names(assays(qf))
+  assay_names <- c("peptides", "peptides_norm", "peptides_log", "proteins", "proteins_norm", "proteins_imptd")
+  dir.create("outputs")
   # Export each assay
   for (assay_name in assay_names) {
     export_assay_with_metadata(qf, assay_name)
