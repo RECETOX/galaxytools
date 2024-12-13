a6e5a40a241e041d5ec9c25d9736a8f8dc595e1e
KristinaGomoryova <gomoryova@sci.muni.cz>
2024-12-13 15:48:24 +0100

    boxplots and heatmap for missing data added

--- tools/scp/utils.R
+++ tools/scp/utils.R
@@ -1,5 +1,9 @@
 library(dplyr)
 library(scp)
+library(tidyr)
+library(tibble)
+
+# Export intermediate results 
 
 # Function to export a single assay with metadata
 export_assay_with_metadata <- function(qf, assay_name) {
@@ -26,4 +30,52 @@ export_all_assays <- function(qf) {
   for (assay_name in assay_names) {
     export_assay_with_metadata(qf, assay_name)
   }
+}
+
+# Plot the QC boxplots
+dir.create("plots")
+
+create_boxplots <- function(scp, i, is_log2, name) {
+  sce <- scp[[i]]
+  
+  assay_data <- as.data.frame(assay(sce)) %>%
+    rownames_to_column("FeatureID")
+
+  col_data <- as.data.frame(colData(scp)) %>%
+    rownames_to_column("SampleID") 
+
+  long_data <- assay_data %>%
+    pivot_longer(
+      cols = -FeatureID,  
+      names_to = "SampleID",  
+      values_to = "Value"  
+    )
+
+  long_data <- long_data %>%
+    left_join(col_data, by = "SampleID")
+
+  if (is_log2 == TRUE){
+    long_data$Value <- log2(long_data$Value)
+  }
+
+  long_data %>%
+    filter(Value != "NaN") %>%
+    ggplot(aes(x = runCol, y = Value, fill = SampleType))+
+    geom_boxplot() +
+    theme_bw() +
+    labs(title = name,
+         x = "Run",
+         y = "Log2 intensity") +
+    theme(axis.text.x = element_text(angle = 45, hjust=1))
+}
+
+# Heatmap
+
+plot_heatmap <- function(scp, i){
+    sce <- scp[[i]]
+    heatmap_mat <- as.matrix(assay(sce))
+    heatmap_mat[is.na(heatmap_mat)] <- 0
+    heatmap_bin <- ifelse(heatmap_mat > 0, 1, 0)
+    colnames(heatmap_bin) <- gsub("Reporter.intensity.", "", colnames(heatmap_bin))
+    heatmap(heatmap_bin, scale = "none",  col = c("white", "black"), labRow = FALSE,  margins = c(10, 5), cexCol = 0.5)
 }
