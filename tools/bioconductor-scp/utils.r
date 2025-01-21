5adfebd17a63e71129d5b11db2419e88cd6d660d
hechth <helge.hecht@recetox.muni.cz>
2025-01-21 17:41:57 +0100

    fixed tests and namespaces

--- tools/bioconductor-scp/utils.r
+++ tools/bioconductor-scp/utils.r
@@ -1,15 +1,10 @@
-library(dplyr)
-library(scp)
-library(tidyr)
-library(tibble)
-
 # Export intermediate results
 # Function to export a single assay with metadata
 export_assay_with_metadata <- function(qf, assay_name) {
     # Extract assay data, row metadata, and col metadata
-    assay_data <- assay(qf[[assay_name]])
-    row_metadata <- as.data.frame(rowData(qf[[assay_name]]))
-    col_metadata <- as.data.frame(colData(qf))
+    assay_data <- SummarizedExperiment::assay(qf[[assay_name]])
+    row_metadata <- as.data.frame(SummarizedExperiment::rowData(qf[[assay_name]]))
+    col_metadata <- as.data.frame(SummarizedExperiment::colData(qf))
     # Combine row metadata with assay data
     export_data <- cbind(RowNames = rownames(assay_data), row_metadata, as.data.frame(assay_data))
     # Save the table to a CSV file
@@ -32,38 +27,38 @@ export_all_assays <- function(qf) {
 # Plot the QC boxplots
 create_boxplots <- function(scp, i, is_log2, name) {
     sce <- scp[[i]]
-    assay_data <- as.data.frame(assay(sce)) %>%
-        rownames_to_column("FeatureID")
-    col_data <- as.data.frame(colData(scp)) %>%
-        rownames_to_column("SampleID")
-    long_data <- assay_data %>%
-        pivot_longer(
+    assay_data <- as.data.frame(SummarizedExperiment::assay(sce)) |>
+        tibble::rownames_to_column("FeatureID")
+    col_data <- as.data.frame(SummarizedExperiment::colData(scp)) |>
+        tibble::rownames_to_column("SampleID")
+    long_data <- assay_data |>
+        tidyr::pivot_longer(
             cols = -FeatureID,
             names_to = "SampleID",
             values_to = "Value"
         )
-    long_data <- long_data %>%
-        left_join(col_data, by = "SampleID")
+    long_data <- long_data |>
+        dplyr::left_join(col_data, by = "SampleID")
     if (is_log2 == TRUE) {
         long_data$Value <- log2(long_data$Value)
     }
-    long_data %>%
-        filter(Value != "NaN") %>%
-        ggplot(aes(x = runCol, y = Value, fill = SampleType)) +
-        geom_boxplot() +
-        theme_bw() +
-        labs(
+    long_data |>
+        dplyr::filter(Value != "NaN") |>
+        ggplot2::ggplot(ggplot2::aes(x = runCol, y = Value, fill = SampleType)) +
+        ggplot2::geom_boxplot() +
+        ggplot2::theme_bw() +
+        ggplot2::labs(
             title = name,
             x = "Run",
             y = "Log2 intensity"
         ) +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+        ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1))
 }
 
 # Heatmap
 plot_heatmap <- function(scp, i) {
     sce <- scp[[i]]
-    heatmap_mat <- as.matrix(assay(sce))
+    heatmap_mat <- as.matrix(SummarizedExperiment::assay(sce))
     heatmap_mat[is.na(heatmap_mat)] <- 0
     heatmap_bin <- ifelse(heatmap_mat > 0, 1, 0)
     colnames(heatmap_bin) <- gsub("Reporter.intensity.", "", colnames(heatmap_bin))
