f7602ea472099fb7dc0ec0bb3d0d2472bc0bd1c8
hechth <helge.hecht@recetox.muni.cz>
2025-01-22 09:14:50 +0100

    lint

--- tools/tables/utils.py
+++ tools/tables/utils.py
@@ -1,38 +1,43 @@
 import argparse
 import pandas as pd
 
+
 class LoadDataAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         file_path, file_extension = values
         file_extension = file_extension.lower()
-        if file_extension == 'csv':
+        if file_extension == "csv":
             df = pd.read_csv(file_path)
-        elif file_extension in ['tsv', 'tabular']:
-            df = pd.read_csv(file_path, sep='\t')
-        elif file_extension == 'parquet':
+        elif file_extension in ["tsv", "tabular"]:
+            df = pd.read_csv(file_path, sep="\t")
+        elif file_extension == "parquet":
             df = pd.read_parquet(file_path)
         else:
             raise ValueError(f"Unsupported file format: {file_extension}")
         setattr(namespace, self.dest, df)
 
+
 def write_csv(df, file_path):
     df.to_csv(file_path, index=False)
 
+
 def write_tsv(df, file_path):
-    df.to_csv(file_path, sep='\t', index=False)
+    df.to_csv(file_path, sep="\t", index=False)
+
 
 def write_parquet(df, file_path):
     df.to_parquet(file_path, index=False)
+
 
 class StoreOutputAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         file_path, file_extension = values
         file_extension = file_extension.lower()
-        if file_extension == 'csv':
+        if file_extension == "csv":
             write_func = write_csv
-        elif file_extension in ['tsv', 'tabular']:
+        elif file_extension in ["tsv", "tabular"]:
             write_func = write_tsv
-        elif file_extension == 'parquet':
+        elif file_extension == "parquet":
             write_func = write_parquet
         else:
             raise ValueError(f"Unsupported file format: {file_extension}")
