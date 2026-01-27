36ed78e3bd86c008ea64e0e6c110909336968dcf
rdurnik <rdurnik@gmail.com>
2026-01-27 11:57:47 +0100

    Script for matching of chemical names to database of relevant chemicals

---
+++ tools/aoptk/aoptk_chemical_matching.py
@@ -0,0 +1,39 @@
+from aoptk.chemical import Chemical
+import argparse
+
+
+def match_chemicals_with_loose_equality(
+    list_of_relevant_chemicals: list[Chemical],
+    normalized_chemicals: list[Chemical],
+) -> list[str]:
+    """Match normalized chemicals with relevant chemicals using loose equality.
+
+    Args:
+        list_of_relevant_chemicals (list[Chemical]): List of relevant chemicals.
+        normalized_chemicals (list[Chemical]): List of normalized chemicals.
+    """
+    relevant_chemicals_names = []
+    for chemical in normalized_chemicals:
+        for relevant_chemical in list_of_relevant_chemicals:
+            if chemical.similar(relevant_chemical):
+                relevant_chemicals_names.append(chemical.name)
+                break
+    return relevant_chemicals_names
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Match chemicals using loose equality")
+    parser.add_argument(
+        "--list_of_relevant_chemicals", required=True, help="List of relevant chemicals"
+    )
+    parser.add_argument(
+        "--normalized_chemicals", required=True, help="List of normalized chemicals"
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    relevant_chemicals_names = match_chemicals_with_loose_equality(
+        args.list_of_relevant_chemicals, args.normalized_chemicals
+    )
